ad3613b561ebd3d5514cdd0935fdd8b8cfd897c8
antonioortegajr <antonioortega@8808.local>
2014-12-11 23:00:35 -0800

    initial commit
    registers the plugin

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Clean up with a wildcard Plugin
+Plugin URI: http://antonioortegajr.com
+Description: Cleaning up MySQL. Delete this plugin to start the clean up.
+add your wildcard string to the uninstall.php file. I use this to clean up after testing plugins
+that do not clean up after themselves.
+USE AT YOUR OWN RISK
+Version: 1.0
+Author: Antonio Ortega Jr
+Author URI: http://antonioortegajr.com
+License: GPL2
+*/
+
+/*
+This file exists only to register the plugin.
+*/
+
+
+?>

9fc6c722f8b016f7cdca24aef0aac95bfd4ddfc0
antonioortegajr <antonioortega@8808.local>
2014-12-11 23:03:25 -0800

    minor change to line breaks
    minor change to line breaks

--- uninstall.php
+++ uninstall.php
@@ -1,19 +1,16 @@
 <?php
 // If uninstall not called from WordPress exit
 if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-exit ();  
- 
+exit ();
+
 // deletes anything it can with 'idx' in the name from the wp_options
 function delete_options_prefixed( $prefix ) {
     global $wpdb;
     $wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '%{$prefix}%'" );
 
-	$dropit = 'wp_posts_idx';
-	
-$wpdb->query("DROP TABLE $dropit");
-	
+
 	}
-delete_options_prefixed( 'idx' );
+delete_options_prefixed( 'YourWildcard' );
 
 
  ?>

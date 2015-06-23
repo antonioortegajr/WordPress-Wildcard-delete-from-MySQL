510f2035545718237a3950ba0499e4e39559d802
antonioortegajr <antonioortegajr@gmail.com>
2015-06-22 23:06:34 -0700

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -3,7 +3,7 @@
 if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 exit ();
 
-// deletes anything it can with 'idx' in the name from the wp_options
+// deletes anything it can with 'YourWildcard' in the name from the wp_options
 function delete_options_prefixed( $prefix ) {
     global $wpdb;
     $wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE '%{$prefix}%'" );
